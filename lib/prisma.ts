d191040006601a230adb5db6b89af1b33cdd2d68
unknown <firmanajah366@gmail.com>
2026-08-11 20:19:30 +0700

    docs: add JSDoc documentation to Prisma singleton in lib/prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,9 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+/**
+ * Singleton instance of PrismaClient to prevent instantiating multiple database connections during hot module replacement.
+ */
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
